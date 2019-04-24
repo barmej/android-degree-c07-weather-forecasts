f1baa5a92324b796e2a514cf8b4aa73ebf6b4def
oza <omar.albelbaisy@gmail.com>
2019-04-25 00:46:33 +0300

    Swap between Primary and Secondary fragments when header clicked

--- app/src/main/java/com/barmej/weatherforecasts/MainActivity.java
+++ app/src/main/java/com/barmej/weatherforecasts/MainActivity.java
@@ -1,9 +1,10 @@
 package com.barmej.weatherforecasts;
 
 import android.os.Bundle;
-
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -31,17 +32,18 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mHoursForecastsRecyclerView;
     private RecyclerView mDaysForecastRecyclerView;
 
+    private boolean mFlip = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Create a new instance from SecondaryWeatherInfoFragment fragment
-        PrimaryWeatherInfoFragment primaryWeatherInfoFragment = new PrimaryWeatherInfoFragment();
+        // Create a new instance from PrimaryWeatherInfoFragment fragment
+        final PrimaryWeatherInfoFragment primaryWeatherInfoFragment = new PrimaryWeatherInfoFragment();
 
         // Create a new instance from SecondaryWeatherInfoFragment fragment
-        SecondaryWeatherInfoFragment secondaryWeatherInfoFragment = new SecondaryWeatherInfoFragment();
+        final SecondaryWeatherInfoFragment secondaryWeatherInfoFragment = new SecondaryWeatherInfoFragment();
 
         // Get an instance of FragmentManager and assign it to mFragmentManager variable
         mFragmentManager = getSupportFragmentManager();
@@ -50,10 +52,29 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
 
         // Add the fragment into the layout by adding it in the FrameLayout with id header
-        fragmentTransaction.add(R.id.header, secondaryWeatherInfoFragment);
+        fragmentTransaction.add(R.id.header, primaryWeatherInfoFragment);
 
         // Commit the changes
         fragmentTransaction.commit();
+
+        // Add OnClickListener to the header FrameLayout to swap between PrimaryWeatherInfoFragment
+        // and SecondaryWeatherInfoFragment
+        findViewById(R.id.header).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment;
+                if (mFlip) {
+                    fragment = secondaryWeatherInfoFragment;
+                } else {
+                    fragment = primaryWeatherInfoFragment;
+                }
+                mFlip = !mFlip;
+                mFragmentManager.beginTransaction()
+                        .setCustomAnimations(R.animator.slide_in_left, R.animator.slide_out_left)
+                        .replace(R.id.header, fragment)
+                        .commit();
+            }
+        });
 
         // Create new HoursForecastAdapter and set it to RecyclerView
         mHoursForecastAdapter = new HoursForecastAdapter(this);
@@ -88,7 +109,6 @@ public class MainActivity extends AppCompatActivity {
         mDaysForecastsAdapter.updateData(daysForecasts);
 
     }
-
 
 
 }
